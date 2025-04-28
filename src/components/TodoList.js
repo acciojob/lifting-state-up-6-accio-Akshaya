1cf03624d2aaa38a034a0fb0eb85728fb6c9b1ce
accio-Akshaya <akshayaganesh0910@gmail.com>
2025-04-28 07:20:06 -0700

    Initial commit

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,7 +3,7 @@ import React from 'react';
 const TodoList = ({ todos, handleComplete }) => {
   return (
     <ul className="space-y-3">
-      {todos.map((todo) => (
+      {todos.map(todo => (
         <li
           key={todo.id}
           data-testid={`todo-${todo.id}`}
@@ -11,17 +11,17 @@ const TodoList = ({ todos, handleComplete }) => {
             todo.completed ? 'bg-green-100 line-through' : 'bg-white'
           }`}
         >
-          <span>{todo.text}</span>
-
-          {!todo.completed && (
-            <button
-              data-testid={`complete-button-${todo.id}`}
-              onClick={() => handleComplete(todo.id)}
-              className="bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-600"
-            >
-              Complete
-            </button>
-          )}
+          <div className="flex justify-between w-full items-center">
+            <span>{todo.text}</span>
+            {!todo.completed && (
+              <button
+                onClick={() => handleComplete(todo.id)}
+                className="bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-600"
+              >
+                Complete
+              </button>
+            )}
+          </div>
         </li>
       ))}
     </ul>
@@ -29,4 +29,3 @@ const TodoList = ({ todos, handleComplete }) => {
 };
 
 export default TodoList;
-
